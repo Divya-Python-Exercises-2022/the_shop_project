ba78c77b1d4abb7cc5b2be92c48138e68e23a100
divya chandran <divya.chandran10@gmail.com>
2022-10-12 12:14:09 +0200

    Add app

---
+++ products/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from products.models import Products
+# Create your views here.
+
+def products_as_template(request):
+    context = {
+        'products': Products.objects.all()
+    }
+    return render(request, 'products.html',context)
+
+    # Alternate way
+    #return render(request, 'products.html', {'products': Products.objects.all()})
+
+
+def product_details(request, product_id):
+    product = Products.objects.get(id=product_id)
+    context = {
+        'products':product
+    }
+    return render(request,'product_details.html', context)
+
+
+def products_as_string(request):
+    products_objs = Products.objects.all()
+
+    list_of_items_str = '<ul>'
+
+    for product in products_objs:
+        list_of_items_str += f'<li>{product.name} - €{product.price}</li>'
+    list_of_items_str += '</ul>'
+
+    result = """
+        <ul>
+            <li>iPhone - 1000€ </li>
+            <li>Samsung - 500€</li>
+            <li>Motorola- 900€</li>
+        </ul>
+    """
+
+    #return HttpResponse("<b>Hi!</b>")
+    return HttpResponse(list_of_items_str)
